a94ae05550bbfc29ba8e15db2d62f3b67d55e3a4
NoOne2246 <christopher.ho@hotmail.com>
2016-05-14 08:48:47 +1000

    Untested Sensor read code
    Code is simple enough, on touch it will send a signal to the teensy on
    which panel is touched. And toggle a state. it does this without delays
    to allow better reading, but may work better with a 1-2ms delay for
    bouncing

---
+++ Sensor_Reader/Sensor_Reader.ino
@@ -0,0 +1,39 @@
+//---------------
+//* Define Pins *
+//---------------
+#define LIM 1018  //The point at which the sensor switches between touch and release
+
+//--------------------
+//* Set up Variables *
+//--------------------
+boolean States[16]; //set states to off
+int Pins[16] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15};
+
+void setup() {
+  //-------------------
+  //* Set Analog Pins *
+  //-------------------
+  for (int i = 0; i < 16; i++){
+    pinMode(Pins[i], INPUT);  //set analog pins for input
+    digitalWrite(Pins[i], HIGH);  //Pull up analog pins
+    States[i] = false;
+  }
+  
+  //-----------------
+  //* Set up Serial *
+  //-----------------
+  Serial.begin(115200);
+}
+
+void loop() {
+  for(int i = 0; i < 16; i++){        //cycle through pins
+    if(analogRead(Pins[i]) < LIM){  //Check if the sensor has detected touch
+      if(States[i] != true){        //If sensor has detected touch, was it previously being touched, if not:
+        States[i] = true;           //Set up so that it's state is being touched
+        Serial.println(i+1);        //Send information on pin touched
+      }
+    } else {                        //If there is no pressure
+      States[i] = false;           //Change state to untouched
+    }
+  }
+}
